3102420890c0c19e48834a1cd9e5e00706fec7dd
Billy Lynch <billy@chainguard.dev>
2024-04-25 12:31:24 -0400

    Enable google, envvar cosign OIDC providers.

--- internal/provider/oidc.go
+++ internal/provider/oidc.go
@@ -6,11 +6,13 @@ import (
 	"github.com/sigstore/cosign/v2/pkg/providers"
 
 	_ "github.com/chainguard-dev/terraform-provider-cosign/internal/provider/interactive"
+	_ "github.com/sigstore/cosign/v2/pkg/providers/envvar"
 	_ "github.com/sigstore/cosign/v2/pkg/providers/filesystem"
 	_ "github.com/sigstore/cosign/v2/pkg/providers/github"
+	_ "github.com/sigstore/cosign/v2/pkg/providers/google"
 )
 
-// An impl that represents github.com/sigstore/cosign/pkg/providers/github.
+// An impl that represents github.com/sigstore/cosign/pkg/providers/*.
 type oidcProvider struct {
 }
 
